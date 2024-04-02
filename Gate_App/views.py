2fd0454ab427cfde4101dff5bf854f479a2a5c2a
gfluser <163880855+gfluser@users.noreply.github.com>
2024-04-02 16:57:17 +0530

    Views.py

--- Gate_App/views.py
+++ Gate_App/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "html")
+    return render(request, 'index.html')
 
 
 def about(request):

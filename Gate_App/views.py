c3c9a0694679ce61f8817dc0ff6e112160afd95d
gfluser <163880855+gfluser@users.noreply.github.com>
2024-04-16 12:18:01 +0530

    views.py

--- Gate_App/views.py
+++ Gate_App/views.py
@@ -1,2 +1,43 @@
 from django.shortcuts import render
 
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def terms(request):
+    return render(request, 'terms.html')
+
+
+def privacy(request):
+    return render(request, 'privacy.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def subscribe(request):
+    return render(request, 'subscribe.html')
+
+
+def blog(request):
+    return render(request, 'grid.html')
+
+
+def details(request):
+    return render(request, 'details.html')
+
+
+def join(request):
+    return render(request, 'join.html')
+
+
+def summary(request):
+    return render(request, 'summary.html')
+
